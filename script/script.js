a4c4b592df31e3f7ff6d383d8eefc52c4d882e8e
Jishan7468 <jishanhossain689065@gmail.com>
2026-05-06 20:22:02 +0600

    made button work

--- script/script.js
+++ script/script.js
@@ -76,4 +76,43 @@ function deleteCard(id){
       section.classList.remove('hidden')
     } 
 }
+function transferToInterview(id){
+  const card = document.getElementById(id)
+  const btn = document.getElementById('card-1-inter')
+  btn.setAttribute('disabled', true)
+  btn.style.backgroundColor = 'light-gray'
+  
 
+  const container = document.getElementById('interview-cards')
+
+  const newDiv = document.createElement('div')
+
+  newDiv.innerHTML = `
+  
+  <div id="card-1" class="space-y-5 bg-white p-7 rounded-3xl b border-l-10 border-gray-200">
+                    <h1 class="font-bold text-xl">Mobile First Corp</h1>
+                    <p class="text-neutral/50">React Native Developer</p>
+                    <div class="flex gap-8 text-neutral/50">
+                        <p>Remote</p>
+                        <li>Full-time</li>
+                        <li>$130,000-$175,000</li>
+                    </div>
+                    <!-- badge container -->
+                    <div>
+                        <button class="btn bg-blue-100 text-black font-normal" disabled>Not Applied</button>
+                    </div>
+                    <p>Build cross-platform mobile applications using React Native. Work on products used by millions of users worldwide.</p>
+                    
+                    
+                    <button class="btn hover:bg-green-300 hover:text-white text-green-500 font-bold border-2 border-green-400">Interview</button>
+                    <button class="btn hover:bg-red-300 hover:text-white text-red-400 font-bold border-2 border-red-400 mb-[20px]">Rejected</button>
+                    
+                    </div>
+                </div>
+                
+  
+  
+  `
+  container.appendChild(newDiv)
+
+}
